5d13e805edde08bc5a8ae2caee80e3dcf3614bb2
AntonBaksa <anton.baksa@elev.ga.lbs.se>
2024-01-11 16:15:03 +0100

    instantiate

--- Assets/Scripts/AntonScripts/SpawnObjects.cs
+++ Assets/Scripts/AntonScripts/SpawnObjects.cs
@@ -6,10 +6,27 @@ public class SpawnObjects : MonoBehaviour
 {
     public GameObject objectToSpawn;
 
+    public float timer = 3f;
+
+    private bool countdown = true;
 
     void Start()
     {
-        Instantiate(objectToSpawn);
-        print("spawn");
+        
+
+    }
+
+    void Update()
+    {
+        if (countdown)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                Instantiate(objectToSpawn, new Vector3(0, 0, 0), Quaternion.identity);//Instatiate object at 0,0,0
+                countdown = false;
+            }
+        }
+        
     }
 }
